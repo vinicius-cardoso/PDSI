1406c702ecf2eef278c3ec03ddc5e66e909d3759
Vinicius Cardoso <vinicius.mct17@gmail.com>
2020-08-09 00:12:05 -0300

    Final do Laboratório 1

--- lab1-expressoes-operacoes-condicoes/1-celsius-e-fahrenheit.c
+++ lab1-expressoes-operacoes-condicoes/1-celsius-e-fahrenheit.c
@@ -1,3 +1,30 @@
+/*
+    1) Celsius e Fahrenheit
+
+    Faca um programa que leia uma temperatura em graus Celsius e a converta para
+    graus Fahrenheit. Fórmula: F = (1.8 * C + 32).
+    O programa deve imprimir o valor (arredondando os valores) em Fahrenheit.
+    OBS: utilize printf("%.0f ", valor); para arredondar.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int converter() {
+    float c, f;
+
+    printf("\nCelsius: ");
+    scanf("%f", &c);
+
+    f = (1.8 * c + 32);
+
+    printf("%0.f", f);
+
+    return 0;
+}
+
 int main() {
-    
+    converter();
+
+    return 0;
 }
