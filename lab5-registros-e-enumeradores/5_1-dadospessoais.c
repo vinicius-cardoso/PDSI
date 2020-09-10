90c70e6a950a0c1be40ad3ab9dda81478c8f9433
vinicius-cardoso <vinicius.mct17@gmail.com>
2020-09-09 22:07:56 -0300

    working

---
+++ lab5-registros-e-enumeradores/5_1-dadospessoais.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+    int i = 0, j = 0;
+    char acao[8], busca[50];
+    
+    struct pessoa{
+        char nome[50];
+        unsigned int idade;
+        float salario;
+    };
+
+    struct pessoa p[4];
+
+    while(1){
+        printf("\nO que deseja fazer? (inserir/mostrar): ");
+        scanf("%s", &acao);
+
+        if(strcmp(acao, "inserir") == 0){
+            if(i >= 4){
+                printf("\nEspaco insuficiente"); 
+            }
+            else{
+                printf("\nInserir: Nome Idade Salario\n");
+                scanf("%s %d %f", &p[i].nome, &p[i].idade, &p[i].salario);
+                printf("Registro %s %d %.2f inserido", p[i].nome, p[i].idade, p[i].salario);
+                i++;         
+            }
+        }
+        if(strcmp(acao, "mostrar") == 0){
+            printf("\nMostrar: Nome\n");
+            scanf("%s", &busca);
+            for(j = 0;j < 5; j++){
+                if(strcmp(p[j].nome, busca) == 0){
+                    printf("\nRegistro %s %d %.2f", p[j].nome, p[j].idade, p[j].salario);
+                    break;
+                }
+                else{
+                    if(j == 4){
+                        printf("\nRegistro ausente");
+                    }
+                }
+            }
+        }
+        else{
+            printf("\nOpcoes: 'inserir' ou 'mostrar'");
+        }
+    }
+
+    return 0;
+}
