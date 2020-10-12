4fbf8523301dadda381ca29529805d0002cae4ae
vinicius-cardoso <vinicius.mct17@gmail.com>
2020-10-12 00:55:00 -0300

    lab 8

--- lab8-ponteiros/8_1-media.c
+++ lab8-ponteiros/8_1-media.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-void media(double vet[], int n, int *i){
-    int media;
+int media(double vet[], int n, int *i){
+    int j;
+    double media, soma = 0;
+
+    for(j = 0; j < n; j++){
+        soma = (soma + vet[j]);
+    }
+
+    media = (soma / n);
 
     return media;
 }
 
 int main(){
-    int n, *i;
+    int n, *i, j;
+    double m, dif, ref = 1000;
+
+    scanf("%d\n", &n);
+    
     double vet[n];
 
-    return 0;
+    for(j = 0; j < n; j++){
+        scanf("%lf", &vet[j]);
+    }
+
+    m = media(vet, n, *i);
+
+    for(j = 0; j < n; j++){
+        dif = abs(m - vet[j]);
+        
+        if(dif < ref){
+            ref = dif;
+        }
+    }
+
+    for(j = 0; j < n; j++){
+        if(abs(m - vet[j]) == ref){
+            *i = j;
+            printf("%d\n", *i);
+            
+            return 0;
+        }
+    }
 }
--- lab8-ponteiros/8_2-primos.c
+++ lab8-ponteiros/8_2-primos.c
@@ -1,13 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void primos(int m, int *p1, int *p2){
-    int primo;
+    int m2 = m, n, ref, cont = 0;
+    int m3, i, p = 0, fim = 0;
 
-    return primo;
+    // primo inferior
+
+    n = m - 1;
+    ref = m - 1;
+
+    for(m2 = ref; m2 >= 2; m2--){
+        for(n = m2; n >= 2; n--){
+            if(m2 % n == 0){
+                cont++;
+            }
+        }
+        if(cont == 1){
+            *p1 = m2;
+            break;
+        }
+
+        cont = 0;
+    }    
+
+    cont = 0;
+
+    // primo superior
+
+    m3 = m + 1;
+
+    while(fim == 0){
+        for(i = 2; i <= m3; i++){
+            if(m3 % i == 0){
+                cont++;
+            }
+        }
+
+        if(cont == 1){
+            *p2 = m3;
+            fim = 1;
+            break;
+        }
+
+        cont = 0;
+
+        m3++;
+    }
+
+    printf("%d\n", *p1);
+    printf("%d", *p2);
 }
 
 int main(){
-    int m, *p1, *p2;
+    int m, p1, p2;
+
+    scanf("%d", &m);
+
+    p2 = m + 1;
+
+    primos(m, &p1, &p2);
 
     return 0;
 }
